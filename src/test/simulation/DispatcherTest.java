34353d072ede2effcf489937a564f2635cfb4c3f
lilBchii <berenice.bianchi04@gmail.com>
2024-12-09 19:05:06 +0100

    add tests for Dispatcher

--- src/test/simulation/DispatcherTest.java
+++ src/test/simulation/DispatcherTest.java
@@ -1,16 +1,33 @@
 package test.simulation;
 
+import main.model.Settler;
+import main.simulation.Dispatcher;
+import main.simulation.Simulation;
 import org.junit.jupiter.api.Test;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DispatcherTest {
 
     @Test
-    void linearDispatch() {
+    void linearDispatchAffectForEverySettler() {
+        Simulation simulation = Simulation.random(26, 5);
+        Dispatcher dispatcher = new Dispatcher(simulation);
+        dispatcher.linearDispatch();
+        for(Settler settler : simulation.getSettlers()) {
+            assertNotNull(settler.getAffectation());
+        }
     }
 
     @Test
-    void maxLEFDispatch() {
+    void maxLEFDispatchAffectForEverySettler() {
+        Simulation simulation = Simulation.random(26, 5);
+        Dispatcher dispatcher = new Dispatcher(simulation);
+        dispatcher.maxLEFDispatch(10);
+        for(Settler settler : simulation.getSettlers()) {
+            assertNotNull(settler.getAffectation());
+        }
     }
 }
